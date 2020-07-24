c80cc9329670578bacf5532fbdbbec7c3ac4ffed
byaction <burakhanefendi@gmail.com>
2020-07-24 23:34:39 +0300

    wordpress

--- wp-content/themes/greengsic/markalar.php
+++ wp-content/themes/greengsic/markalar.php
@@ -129,7 +129,7 @@ get_header(); ?>
             <div class="col-md-4">
                 <div class="teaser with_border text-center">
                     <div class="teaser_icon highlight size_small">
-                        <img src="/wp-content/uploads/2020/07/akasya_su2.jpg">
+                        <img src="/wp-content/uploads/2020/07/akasyasu.jpg">
                     </div>
                     <h4 class="text-uppercase text-center">
                         Su
