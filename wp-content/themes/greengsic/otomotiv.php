e1ab1dd86837f0e6355181ef13417980e9d08bd2
byaction <burakhanefendi@gmail.com>
2020-08-16 22:41:53 +0300

    wordpress

--- wp-content/themes/greengsic/otomotiv.php
+++ wp-content/themes/greengsic/otomotiv.php
@@ -26,15 +26,24 @@ get_header(); ?>
 <section class="ls page_portfolio section section_padding_bottom_10 columns_padding_25 columns_margin_bottom_30">
     <div class="container">
         <div class="row">
-            <div class="col-md-12" style="text-align: center;">
-
+            <div class="col-md-6 col-md-push-6">
+                <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full'); ?>
+                <img src="<?php echo $thumb['0'];?>" alt="">
+            </div>
+            <div class="col-md-6 col-md-pull-6">
+                <h2 class="section_header thin">
+                    <?php the_title();?>
+                </h2>
                 <p><?php the_content();?></p>
                 <?php endwhile; else : ?>
                 <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
             </div>
         </div>
-        <?php endif; ?>
     </div>
+    <?php endif; ?>
+</section>
+<section class="ls page_portfolio section section_padding_bottom_10 columns_padding_25 columns_margin_bottom_30">
+
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -42,7 +51,7 @@ get_header(); ?>
 
                     <?php
                     $loop = new WP_Query( array(
-                            'post_type' => 'otomotiv',
+                            'post_type' => 'insaat',
                             'posts_per_page' => -1
                         )
                     );
@@ -74,8 +83,9 @@ get_header(); ?>
 
         </div>
     </div>
-
 </section>
+
+
 
 
 <?php get_footer() ?>
