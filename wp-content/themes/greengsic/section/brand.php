db37c871a24265e366c4cfee244682365b5cb00a
byaction <burakhanefendi@gmail.com>
2020-08-28 21:13:57 +0300

    wordpress

--- wp-content/themes/greengsic/section/brand.php
+++ wp-content/themes/greengsic/section/brand.php
@@ -8,32 +8,32 @@
 
             <!--========= 2nd slide =========-->
             <div class="item active">
-                <div class="col-xs-2 col-sm-6 col-md-2 range_slides_item_image">
+                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
                     <a href="http://www.naturabagno.com"><img src="/wp-content/uploads/2020/08/naturabagno-07.jpg" alt="slider 02" /></a>
                 </div>
             </div>
             <div class="item">
-                <div class="col-xs-2 col-sm-6 col-md-2 range_slides_item_image">
+                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
                     <a href="http://www.akasyasu.com"><img src="/wp-content/uploads/2020/08/akasya-ogo1111.jpg" alt="slider 04" /></a>
                 </div>
             </div>
             <div class="item">
-                <div class="col-xs-2 col-sm-6 col-md-2 range_slides_item_image">
+                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
                     <a href="http://www.idealcelikenerji.com"><img src="/wp-content/uploads/2020/08/idealcelikenerji-1.jpg" alt="slider 04" /></a>
                 </div>
             </div>
             <div class="item">
-                <div class="col-xs-2 col-sm-6 col-md-2 range_slides_item_image">
+                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
                     <a href="http://www.petramarble.com.tr"><img src="/wp-content/uploads/2020/08/petra_marble.jpg" alt="slider 04" /></a>
                 </div>
             </div>
             <div class="item">
-                <div class="col-xs-2 col-sm-6 col-md-2 range_slides_item_image">
+                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
                     <a href="http://www.statecorps.com.tr"><img src="/wp-content/uploads/2020/08/state_corps_turkey.jpg" alt="slider 04" /></a>
                 </div>
             </div>
             <div class="item">
-                <div class="col-xs-2 col-sm-6 col-md-2 range_slides_item_image">
+                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
                     <a href="http://petrayapi.com/"><img src="/wp-content/uploads/2020/08/petrayapı_logo-06.jpg" alt="slider 04" /></a>
                 </div>
             </div>
