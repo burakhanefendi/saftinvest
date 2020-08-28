82f01c8a86ed7ed00319a9cceefbb2bf651269a2
byaction <burakhanefendi@gmail.com>
2020-08-28 20:13:22 +0300

    wordpress

--- wp-content/themes/greengsic/section/brand.php
+++ wp-content/themes/greengsic/section/brand.php
@@ -1,5 +1,5 @@
 <section class="brand-carousel">
-    <div id="adv_range_4_columns_carousel" class="carousel slide four_shows_one_move range_slides_carousel_wrapper swipe_x" data-ride="carousel" data-duration="3000" data-interval="1500" data-pause="hover">
+    <div id="adv_range_6_columns_carousel" class="carousel slide four_shows_one_move range_slides_carousel_wrapper swipe_x" data-ride="carousel" data-duration="3000" data-interval="1500" data-pause="hover">
 
         <!--========= Wrapper for slides =========-->
         <div class="carousel-inner range_slides_carousel_inner" role="listbox">
@@ -8,32 +8,32 @@
 
             <!--========= 2nd slide =========-->
             <div class="item active">
-                <div class="col-xs-12 col-sm-6 col-md-3 range_slides_item_image">
+                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
                     <a href="http://www.naturabagno.com"><img src="/wp-content/uploads/2020/08/naturabagno-07.jpg" alt="slider 02" /></a>
                 </div>
             </div>
             <div class="item">
-                <div class="col-xs-12 col-sm-6 col-md-3 range_slides_item_image">
+                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
                     <a href="http://www.akasyasu.com"><img src="/wp-content/uploads/2020/08/akasya-ogo1111.jpg" alt="slider 04" /></a>
                 </div>
             </div>
             <div class="item">
-                <div class="col-xs-12 col-sm-6 col-md-3 range_slides_item_image">
+                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
                     <a href="http://www.idealcelikenerji.com"><img src="/wp-content/uploads/2020/08/idealcelikenerji-1.jpg" alt="slider 04" /></a>
                 </div>
             </div>
             <div class="item">
-                <div class="col-xs-12 col-sm-6 col-md-3 range_slides_item_image">
+                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
                     <a href="http://www.petramarble.com.tr"><img src="/wp-content/uploads/2020/08/petra_marble.jpg" alt="slider 04" /></a>
                 </div>
             </div>
             <div class="item">
-                <div class="col-xs-12 col-sm-6 col-md-3 range_slides_item_image">
+                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
                     <a href="http://www.statecorps.com.tr"><img src="/wp-content/uploads/2020/08/state_corps_turkey.jpg" alt="slider 04" /></a>
                 </div>
             </div>
             <div class="item">
-                <div class="col-xs-12 col-sm-6 col-md-3 range_slides_item_image">
+                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
                     <a href="http://petrayapi.com/"><img src="/wp-content/uploads/2020/08/petrayapı_logo-06.jpg" alt="slider 04" /></a>
                 </div>
             </div>
