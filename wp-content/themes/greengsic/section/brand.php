f5bd44249870539efad285edc56d5d2c3d71b418
byaction <burakhanefendi@gmail.com>
2020-09-01 11:05:53 +0300

    wordpress

--- wp-content/themes/greengsic/section/brand.php
+++ wp-content/themes/greengsic/section/brand.php
@@ -1,22 +1,22 @@
 <section class="brand-carousel">
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-sm-6 col-md-2">
+            <div class="col-xs-6 col-sm-6 col-md-2">
                 <a href="http://www.naturabagno.com"><img src="/wp-content/uploads/2020/08/saffinvest_natura_bagno.jpg" alt="slider 02" /></a>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-2">
+            <div class="col-xs-6 col-sm-6 col-md-2">
                 <a href="http://www.akasyasu.com"><img src="/wp-content/uploads/2020/08/saffinvest_akasyasu.jpg" alt="slider 04" /></a>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-2">
+            <div class="col-xs-6 col-sm-6 col-md-2">
                 <a href="http://www.idealcelikenerji.com"><img src="/wp-content/uploads/2020/08/saffinvest_ideaIteknik.jpg" alt="slider 04" /></a>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-2">
+            <div class="col-xs-6 col-sm-6 col-md-2">
                 <a href="http://www.petramarble.com.tr"><img src="/wp-content/uploads/2020/08/saffinvest_petra_mermer.jpg" alt="slider 04" /></a>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-2">
+            <div class="col-xs-6 col-sm-6 col-md-2">
                 <a href="http://www.statecorps.com.tr"><img src="/wp-content/uploads/2020/08/saffinvest_state-corps-turkey.jpg" alt="slider 04" /></a>
             </div>
-            <div class="col-xs-12 col-sm-6 col-md-2">
+            <div class="col-xs-6 col-sm-6 col-md-2">
                 <a href="http://petrayapi.com/"><img src="/wp-content/uploads/2020/08/saffinvest_petra-Iogo.jpg" alt="slider 04" /></a>
             </div>
         </div>
