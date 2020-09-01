66f0e27f7820ca68f0c98212f8d8926934bc6ca0
byaction <burakhanefendi@gmail.com>
2020-09-01 11:02:10 +0300

    wordpress

--- wp-content/themes/greengsic/section/brand.php
+++ wp-content/themes/greengsic/section/brand.php
@@ -1,59 +1,24 @@
 <section class="brand-carousel">
-    <div id="adv_range_6_columns_carousel" class="carousel slide six_shows_one_move range_slides_carousel_wrapper swipe_x" data-ride="carousel" data-duration="3000" data-interval="1500" data-pause="hover">
-
-        <!--========= Wrapper for slides =========-->
-        <div class="carousel-inner range_slides_carousel_inner" role="listbox">
-
-            <!--========= 1st slide =========-->
-
-            <!--========= 2nd slide =========-->
-            <div class="item active">
-                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
-                    <a href="http://www.naturabagno.com"><img src="/wp-content/uploads/2020/08/saffinvest_natura_bagno.jpg" alt="slider 02" /></a>
-                </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-2">
+                <a href="http://www.naturabagno.com"><img src="/wp-content/uploads/2020/08/saffinvest_natura_bagno.jpg" alt="slider 02" /></a>
             </div>
-            <div class="item">
-                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
-                    <a href="http://www.akasyasu.com"><img src="/wp-content/uploads/2020/08/saffinvest_akasyasu.jpg" alt="slider 04" /></a>
-                </div>
+            <div class="col-xs-12 col-sm-6 col-md-2">
+                <a href="http://www.akasyasu.com"><img src="/wp-content/uploads/2020/08/saffinvest_akasyasu.jpg" alt="slider 04" /></a>
             </div>
-            <div class="item">
-                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
-                    <a href="http://www.idealcelikenerji.com"><img src="/wp-content/uploads/2020/08/saffinvest_ideaIteknik.jpg" alt="slider 04" /></a>
-                </div>
+            <div class="col-xs-12 col-sm-6 col-md-2">
+                <a href="http://www.idealcelikenerji.com"><img src="/wp-content/uploads/2020/08/saffinvest_ideaIteknik.jpg" alt="slider 04" /></a>
             </div>
-            <div class="item">
-                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
-                    <a href="http://www.petramarble.com.tr"><img src="/wp-content/uploads/2020/08/saffinvest_petra_mermer.jpg" alt="slider 04" /></a>
-                </div>
+            <div class="col-xs-12 col-sm-6 col-md-2">
+                <a href="http://www.petramarble.com.tr"><img src="/wp-content/uploads/2020/08/saffinvest_petra_mermer.jpg" alt="slider 04" /></a>
             </div>
-            <div class="item">
-                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
-                    <a href="http://www.statecorps.com.tr"><img src="/wp-content/uploads/2020/08/saffinvest_state-corps-turkey.jpg" alt="slider 04" /></a>
-                </div>
+            <div class="col-xs-12 col-sm-6 col-md-2">
+                <a href="http://www.statecorps.com.tr"><img src="/wp-content/uploads/2020/08/saffinvest_state-corps-turkey.jpg" alt="slider 04" /></a>
             </div>
-            <div class="item">
-                <div class="col-xs-12 col-sm-6 col-md-2 range_slides_item_image">
-                    <a href="http://petrayapi.com/"><img src="/wp-content/uploads/2020/08/saffinvest_petra-Iogo.jpg" alt="slider 04" /></a>
-                </div>
+            <div class="col-xs-12 col-sm-6 col-md-2">
+                <a href="http://petrayapi.com/"><img src="/wp-content/uploads/2020/08/saffinvest_petra-Iogo.jpg" alt="slider 04" /></a>
             </div>
-
-
         </div>
-
-        <!--======= Navigation Buttons =========-->
-
-        <!--======= Left Button =========-->
-        <a class="left carousel-control range_slides_carousel_control_left range_adv_left" href="#adv_range_4_columns_carousel" role="button" data-slide="prev">
-            <i class="rt-icon2-chevron-left"></i>
-            <span class="sr-only">Previous</span>
-        </a>
-
-        <!--======= Right Button =========-->
-        <a class="right carousel-control range_slides_carousel_control_right range_adv_right" href="#adv_range_4_columns_carousel" role="button" data-slide="next">
-            <i class="rt-icon2-chevron-right"></i>
-            <span class="sr-only">Next</span>
-        </a>
-
     </div>
 </section>
